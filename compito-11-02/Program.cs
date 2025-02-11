756f52a0b867a8237c1f2c58e6503497a6ec17cd
camillazicari <camyzicari01@gmail.com>
2025-02-11 14:47:09 +0100

    fine

--- compito-11-02/Program.cs
+++ compito-11-02/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using compito_11_02.models;
+
+var persona1 = new Persona()
+{
+    Nome = "Camilla",
+    Cognome = "Zicari",
+    Eta = 23,
+};
+
+Console.WriteLine(persona1.ScriviNome());
+Console.WriteLine(persona1.ScriviCognome());
+Console.WriteLine(persona1.ScriviEta());
---
+++ compito-11-02/models/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compito_11_02.models
+{
+    public class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+        public string ScriviNome()
+        {
+            return Nome;
+        }
+
+        public string ScriviCognome()
+        {
+            return Cognome;
+        }
+
+        public int ScriviEta()
+        {
+            return Eta;
+        }
+    }
+}
